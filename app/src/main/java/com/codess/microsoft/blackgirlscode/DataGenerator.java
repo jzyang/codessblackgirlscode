29fecda39331b9cdd44617ee1c1555f5ab319466
jzyang <jyang@soasta.com>
2014-10-11 16:14:03 -0700

    More changes

--- app/src/main/java/com/codess/microsoft/blackgirlscode/DataGenerator.java
+++ app/src/main/java/com/codess/microsoft/blackgirlscode/DataGenerator.java
@@ -40,7 +40,7 @@ public class DataGenerator {
     String[] hobbies = {"swimming, running", "camping, hunting", "skiing, snowboarding", "sleeping, eating", "traveling, dancing"};
     String[] goals = {"Build an Android app", "Build a web app", "Build something to help the Red Cross", "Learn backend development", "Learn data science"};
 
-    for (int i = 0; i <= 10; i++) {
+    for (int i = 0; i < 5; i++) {
       Person person = new Person();
       person.setAge((int)(Math.random() * 50 + 1));
       person.setName(names[i]);
